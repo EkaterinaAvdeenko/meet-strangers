0b14654dd280afaeb21b7b113ad2216f66087e2c
Avdeenko Ekaterina <avdeenqoeka@gmail.com>
2022-10-19 17:11:22 +0700

    Meet_strangers_AvdeenkoEkaterina

--- src/main/java/com/epam/rd/autotasks/meetstrangers/HelloStrangers.java
+++ src/main/java/com/epam/rd/autotasks/meetstrangers/HelloStrangers.java
@@ -1,10 +1,20 @@
 package com.epam.rd.autotasks.meetstrangers;
 
-import java.io.IOException;
+import java.util.Scanner;
 
 public class HelloStrangers {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int strangers = Integer.valueOf(scanner.nextLine());
+        if (strangers < 0) {
+            System.out.println("Seriously? Why so negative?");
+        } else if (strangers == 0) {
+            System.out.println("Oh, it looks like there is no one here");
+        } else {
+            for (int i = 0; i < strangers; i++) System.out.println("Hello, " + scanner.nextLine());
+        }
+    }
         //Write a program, asks for a number - amount of strangers to meet.
         //Then reads stranger names line by line and prints line by line "Hello, ...".
     }
-}
+
